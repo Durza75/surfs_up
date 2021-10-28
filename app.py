fa0cb71ab1fa714d45eeafc46a8aeb21b31cdd4e
Durza75 <estrobach@gwu.edu>
2021-10-28 06:15:34 -0400

    9.5.2

--- app.py
+++ app.py
@@ -5,3 +5,49 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello world'
 
+## 9.5.1 SETUP THE DATABASE AND FLASK
+
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
+
+app = Flask(__name__)
+
+import app
+
+print("example __name__ = %s", __name__)
+
+if __name__ == "__main__":
+    print("example is being run directly.")
+else:
+    print("example is being imported")
+
+## 9.5.2 CREATE THE WELCOME ROUTE
+
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+
+# python app.py
